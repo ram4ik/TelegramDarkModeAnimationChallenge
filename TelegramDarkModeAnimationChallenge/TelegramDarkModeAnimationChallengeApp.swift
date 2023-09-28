5dc575e78443ffb5be65655e30f3af00afaf8e87
Ramill Ibragimov <146063206+ribragimov-mwb@users.noreply.github.com>
2023-09-28 14:13:01 +0300

    Initial Commit

---
+++ TelegramDarkModeAnimationChallenge/TelegramDarkModeAnimationChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TelegramDarkModeAnimationChallengeApp.swift
+//  TelegramDarkModeAnimationChallenge
+//
+//  Created by Ramill Ibragimov on 9/28/23.
+//
+
+import SwiftUI
+
+@main
+struct TelegramDarkModeAnimationChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
